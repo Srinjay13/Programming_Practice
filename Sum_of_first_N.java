32904e91e97f02ab18055c79e0a3e80bafb413a7
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-05-13 11:49:46 +0530

    Update Sum_of_first_N.java

--- Sum_of_first_N.java
+++ Sum_of_first_N.java
@@ -2,10 +2,12 @@ import java.util.*;
 public class Sum_of_first_N{
     public static int sum(int n)
     {
+        //Base case
         if(n==1)
         {
             return n;
         }
+        //Recursive step
         return n+sum(n-1);
     }
     public static void main(String[] args) {
