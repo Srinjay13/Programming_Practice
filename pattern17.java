efaaa97e1cb745097a7610f2952e366d40a7a73d
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-06-05 19:06:43 +0530

    Day22

---
+++ pattern17.java
@@ -0,0 +1,26 @@
+/* Write a program to print this pattern: (FLOYD'S TRIANGLE) 
+       1
+       2  3
+       4  5  6
+       7  8  9  10
+       11 12 13 14 15
+
+*/
+
+import java.util.*;
+public class pattern17
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num=1;
+        //outer loop
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
