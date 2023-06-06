db161c7c686273a9e761f38c6542460864d3b3b6
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-06-06 06:56:38 +0530

    Day23

---
+++ pattern14.java
@@ -0,0 +1,49 @@
+
+/* Write a program to print this pattern:  
+     *********
+      *     *
+       *   *
+        * *
+         *
+    
+
+*/
+
+import java.util.*;
+public class pattern14
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //outer loop
+        for (int i = n; i >=1; i--) {
+            // int c=1;
+            if(i!=n)
+            {
+                for (int spaces = 1; spaces <= n-i; spaces++) {
+                    System.out.print("  ");
+                }
+                for (int j = 1; j <=2*i-1 ; j++) {
+                    if(j%2!=0 && ((j==1) || (j==2*i-1)))
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+
+                }
+            }
+            else
+            {
+                for (int spaces = 1; spaces <= n-i; spaces++) {
+                    System.out.print("  ");
+                }
+                for (int j = 1; j <=2*i-1 ; j++) {
+                        System.out.print("* ");
+                }
+            }
+        
+            
+            
+            System.out.println();
+        }
+    }
+}
