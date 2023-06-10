0e020ab5f48e256ceb61dd40e0497d2b291b98a2
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-06-11 00:30:17 +0530

    Day28

---
+++ pattern2.java
@@ -0,0 +1,24 @@
+/* Write a program to print this pattern:  
+*
+**
+***
+****
+*****
+
+*/
+
+import java.util.*;
+public class pattern2
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //outer loop
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
