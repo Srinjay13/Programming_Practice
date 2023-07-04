0be0a8ac0acab61a743daffebd07becbcd501818
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-07-05 00:12:00 +0530

    Day33

---
+++ Capitalize_first_and_last.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Capitalize_first_and_last {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String st=sc.nextLine();
+        
+    }
+}
