40fd2af5085ebf35ac7f477aa2a7d55d7e42454f
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-08-08 08:55:30 +0530

    Day55

---
+++ Find_middle_index_in_arr.java
@@ -0,0 +1,19 @@
+public class Find_middle_index_in_arr {
+    public static void main() {
+        /*int sum = 0;
+        
+        for(int i : a) sum += i;
+
+        int leftSum = 0;
+        int rightSum = sum;
+
+        for(int i = 0; i < a.length; i++){
+            rightSum -= a[i];
+
+            if(rightSum == leftSum) return i;
+
+            leftSum += a[i];
+        }
+        return -1;*/
+    }
+}
