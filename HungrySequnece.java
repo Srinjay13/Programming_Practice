7c61914ae6538e5aae763cfb59c60f4768d2ad98
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-09-18 11:20:27 +0530

    Day86

---
+++ HungrySequnece.java
@@ -0,0 +1,17 @@
+/*Iahub and Iahubina went to a date at a luxury restaurant. Everything went fine until paying for the food. Instead of money, the waiter wants Iahub to write a Hungry sequence consisting of n integers.
+
+A sequence a1, a2, ..., an, consisting of n integers, is Hungry if and only if:
+
+Its elements are in increasing order. That is an inequality ai < aj holds for any two indices i, j (i < j).
+For any two indices i and j (i < j), aj must not be divisible by ai.
+Iahub is in trouble, so he asks you for help. Find a Hungry sequence with n elements.
+
+*/
+public class HungrySequnece{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            System.out.println(n+i);
+        }
+    }
+}
