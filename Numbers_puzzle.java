8c56276c411af2498f6c71fe28c317e4770da4ca
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-09-29 19:00:52 +0530

    Day90

---
+++ Numbers_puzzle.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Numbers_puzzle {
+    public static void main(String[] args) {
+        int[] geeks = {1,2,3,4,5};
+ 
+        // Conversion of array to ArrayList
+        // using Arrays.asList
+        List al = Arrays.asList(geeks);
+        System.out.println(al);
+        // Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+        // int[] arr=new int[n];
+        // for(int i=0;i<n;i++){
+        //     arr[i]=sc.nextInt();
+        // }
+        // Arrays.sort(arr);
+        // System.out.println(Arrays.toString(arr));
+        // int diff=0;
+        // for(int i=0;i<n-1;i++){
+        //     diff+=Math.abs(arr[i]-arr[i+1]);
+        //     // System.out.println(diff);
+        // }
+        // System.out.println(diff);
+        
+
+    }
+}
